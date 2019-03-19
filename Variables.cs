9a3d5c439c33c737ba7d7af53363bdf04081b8de
nitr8 <nitr8@nfye.com>
2019-03-19 07:54:05 +0100

    updated chrome driver & MS Test SDK

--- Variables.cs
+++ Variables.cs
@@ -2,7 +2,7 @@ namespace ST.Selenium.Test
 {
     class Variables
     {
-        public static readonly string useBrowser = "ChromeService";
+        public static readonly string useBrowser = "Chrome";
         public const string googleURL = "https://google.com";
         public const string ieLog = "\\output\\iedriver2.log";
         public const string scPath = "screenshots";
